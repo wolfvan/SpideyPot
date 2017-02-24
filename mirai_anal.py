6fd2d62fa549e6c0134752380392180f6fccf972
wolfvan <joanbtl@hotmail.com>
2017-02-25 00:18:00 +0100

    Update mirai_anal.py

--- mirai_anal.py
+++ mirai_anal.py
@@ -3,7 +3,12 @@ import os
 
 global BINARIES
 global BIN_FUN 
+
+
+BINARIES = []
 BIN_FUN = {}
+
+
 
 def open_bin(b):
 	r = r2pipe.open(b)
@@ -11,10 +16,21 @@ def open_bin(b):
 
 def anal_bin(b):
 	anal = b.cmd("aa")
+	
+	
+def extract_fun(b)
+	BIN_FUN[b] = b.cmd("f")
 
 
 def extract_bin():
 	os.listdir("/home/mirais/bin/")
+	
+
+def compare_name_functions():
+	for i in BIN_FUN.keys():
+		for j in BIN_FUN.values()
+			list .append(j)
+		
 
 
 def main()
@@ -22,6 +38,8 @@ def main()
 	for b in BINARIES:
 		openbin = open_bin(b)
 		analbin = anal_bin(openbin)
+		extract_fun(analbin)
+	compare_name_functions()
 
 
 if __name__ == "__main__":
