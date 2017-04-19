ed420513525d88344cc0255f67094f27ad27a9b6
wolfvan <joanbtl@hotmail.com>
2017-04-19 18:43:09 +0200

    Registry modified

--- v1.py
+++ v1.py
@@ -5,7 +5,7 @@ import os
 import r2pipe
 import json
 
-def main()
+def main():
 	chk()
 
 
@@ -16,12 +16,25 @@ def chk():
 		for dir in dirs:
 			a1 = os.listdir(dir)
 			for i in a1:
-				if i not in binaries:
+				if in_registry(i):
 					anal_bin(dir+i)
-					binaries.append(i)
+					
 			
 		sleep(60)
 
+def in_registry(i):
+	flag = False
+	f = open("registry.log", "r")
+	for line in f:
+		if i in line:
+			flag = True
+	f.close
+	if flag == True:
+		f1 = open("registry.log", "w")
+		f1.write(i)
+		f1.close()
+
+	return flag
 
 def anal_bin(bin):
 	anl = r2pipe.open(bin)
@@ -29,7 +42,8 @@ def anal_bin(bin):
 	inforj = json.loads(infor)
 	ctime = generate_time
 	infor_time = append_time(inforj, ctime)
-	export_json(infor)
+	#export_json(infor)
+	write_json(infor)
 
 
 def generate_time(bin):
@@ -37,6 +51,9 @@ def generate_time(bin):
 	ts = time.time()
 	st = datetime.datetime.fromtimestamp(ts).strftime(“%Y-%m-%d %H:%M:%S”)
 	return st
+
+def write_json(infor):
+	f = open("write_json")
 
 def append_time(infor, ctime):
 	with open(infor, mode = “w”, encoding = “utf-8”) as j:
@@ -47,8 +64,9 @@ def append_time(infor, ctime):
 	return j
 
 def export_json(infor):
+
 	res.index(index= “binaries_honeypot”, doc_type= “json”, id=1, body=infor)
 
 
 if __init__ == "__main__":
-	main()
+	main()	
